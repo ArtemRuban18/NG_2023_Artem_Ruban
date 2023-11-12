bb2f79c9cdd512d7a6dea4db28d292bd19e985d2
Artem <artemruban762@gmail.com>
2023-11-12 16:05:11 +0200

    Added task 4 of Lection 1 and task 3 Lection 2

--- Lection 1/task_4.py
+++ Lection 1/task_4.py
@@ -1,3 +1,30 @@
 import math
 def calculator():
-    pass
+    first_num = float(input("Enter first number: "))
+    second_num = float(input("Enter second number: "))
+    match input("Avaliable operations : +, -, *, /, **, root. Choose one of them: "):
+        case '+':
+            res = first_num + second_num
+            return f"{first_num} + {second_num} = {res}"
+        case '-':
+            res = first_num - second_num
+            return f"{first_num} - {second_num} = {res}"
+        case '*':
+            res = first_num * second_num
+            return f"{first_num} * {second_num} = {res}"
+        case '/':
+            if second_num == 0:
+                print("Cannot be divided by 0!")
+            else:
+                res = first_num / second_num
+                return f"{first_num} / {second_num} = {res}"
+        case '**':
+            res = pow(first_num,second_num)
+            return f"{first_num} ^{second_num} = {res}"
+        case 'root':
+            res = pow(first_num,1/second_num)
+            return f"{first_num} - {second_num} = {res}"
+
+result = calculator()
+print(result)
+        
--- Lection 2/task_3.py
+++ Lection 2/task_3.py
@@ -3,17 +3,21 @@ for num in range(1,end_num+1):
     dividers = [str(divider) for divider in range(1,num +1) if num % divider == 0]
     dividers_string = ", ".join(dividers)
     print(f"{num}     |     {dividers_string}")
+    
 
-def simple_num(n):
-    if n < 1:
+def simple_num(number):
+    if number < 0:
         return False
-    for i in range(2,n):
-        if n % i == 0:
+    for divider in range(2,number):
+        if number % divider == 0:
             return False
-        else:
-            return True
-n = int(input("Enter a number: "))
-if simple_num(n):
-    print(f'{n} - natural number')
+    return True
+    
+number = int(input("Enter number: "))
+result = simple_num(number)
+arrayOfSimpleNumber = [number for number in range(2, number) if simple_num(number)]
+if result:
+    print(f"{number} is simple number")
+    print(f"List of simple number: {arrayOfSimpleNumber}")
 else:
-    print(f'{n} - not natural number')
+    print(f"{number} is not simple number")
